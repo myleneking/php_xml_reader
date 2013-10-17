be04f82386dac0e13cbb846bc9d79a0adca3680e
Mylene King <mylene.king@gmail.com>
2013-10-17 20:48:51 +0800

    master: setup laravel project.

---
+++ script/composer_update.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -x
+#******************************#
+# USAGE:                       #
+# ./composer_update.sh project #
+#******************************#
+
+cd ./../php_xml_reader
+
+# download composer.phar if does not exist.
+if [ ! -f composer.phar ];
+then
+   echo "composer.phar does not exist."
+   curl -s https://getcomposer.org/installer | php
+fi
+
+# get libraries and dependencies.
+php composer.phar update --no-scripts
+
+#create .gitignore
+touch ../.gitignore
+echo ${PROJECT}"/composer.lock" >> ../.gitignore
+echo ${PROJECT}"/composer.phar" >> ../.gitignore
+echo ${PROJECT}"/vendor" >> ../.gitignore
